166777adbbb8237b1b83ff995d7fd51492c3a5f1
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 22:35:44 +0330

    Return 406 error code instead of 400

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+const errors = require('../config/errors');
+
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) return res.status(406).send(errors.ACCESS_DENIED);
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded; 
+    req.user = jwt.verify(token, config.get('jwtPrivateKey'));
     next();
   }
   catch (ex) {
-    res.status(400).send('Invalid token.');
+    res.status(406).send(errors.ACCESS_DENIED);
   }
 }
