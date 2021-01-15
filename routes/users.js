d277be1f121bbf4a0eafb36e2335be5237609c44
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 22:36:42 +0330

    Add login API

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,11 @@ const _ = require('lodash');
 const express = require('express');
 
 const {User} = require('../models/user');
-const errors = require('../config/Errors');
+const errors = require('../config/errors');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
   const {email} = req.body;
   const salt = await bcrypt.genSalt();
 
@@ -25,15 +25,19 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   let {email, password} = req.body;
-  const salt = await bcrypt.genSalt();
-  password = await bcrypt.hash(password, salt);
-  console.log(password);
 
-  let user = await User.findOne({ email, password});
+  let user = await User.findOne({ email});
   if (!user) return res.status(406).send(errors.USER_DOSE_NOT_EXIST);
 
-  const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result == true) {
+      const token = user.generateAuthToken();
+      res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+    } else {
+     res.send('Incorrect password');
+    }
+  });
+  
 });
 
 module.exports = router; 
