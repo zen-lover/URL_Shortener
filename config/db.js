d08241d46449753dfddad4c79a4dd6a171941d3c
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 22:34:48 +0330

    Delete comments in db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-const { promisify } = require('util');
-// const asyncRedis = require("async-redis");
 const redis = require('redis');
-
 const config = require('config');
 const db = config.get('mongoURI');
 
@@ -18,22 +15,6 @@ const connectDb = async () => {
     process.exit(1);
   }
 };
-
-// const client = asyncRedis.createClient();
- 
-// client.on("error", function (err) {
-//     console.log("Error " + err);
-// });
- 
-// const asyncBlock = async () => {
-//   await client.set("string key", "string val");
-//   const value = await client.get("string key");
-//   console.log(value);
-//   await client.flushall("string key");
-// };
-
-
-// Create Redis Client
 let client = redis.createClient();
 
 client.on('connect', function(){
