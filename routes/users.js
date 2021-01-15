65b4773f30f11ae970015d8fbbb50217afe552c5
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 19:58:33 +0330

    Fix and refactore urls

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,36 @@
-const auth = require('../middleware/auth');
 const jwt = require('jsonwebtoken');
-const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const {User, validate} = require('../models/user');
-const mongoose = require('mongoose');
 const express = require('express');
+
+const {User} = require('../models/user');
+const errors = require('../config/Errors');
+
 const router = express.Router();
 
-router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
-});
-
 router.post('/', async (req, res) => {
-//   const { error } = validate(req.body); 
-//   if (error) return res.status(400).send(error.details[0].message);
+  const {email} = req.body;
+  const salt = await bcrypt.genSalt();
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered.');
+  let user = await User.findOne({ email });
+  if (user) return res.status(406).send(errors.USER_ALREADY_REGISTERED);
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
-  const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
+
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.post('/login', async (req, res) => {
+  let {email, password} = req.body;
+  const salt = await bcrypt.genSalt();
+  password = await bcrypt.hash(password, salt);
+  console.log(password);
+
+  let user = await User.findOne({ email, password});
+  if (!user) return res.status(406).send(errors.USER_DOSE_NOT_EXIST);
 
   const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
