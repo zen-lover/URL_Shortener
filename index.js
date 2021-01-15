bf5dafec4fa941b90aec5f2c80a9aad7a8376d56
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 18:50:51 +0330

    Refactore index.js file

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const config = require('config');
-const { connectDB, client } = require('./config/db');
-const mongoose = require('mongoose');
-const DeviceDetector = require("device-detector-js");
 const express = require('express');
-const app = express();
+const config = require('config');
 
+const { connectDb } = require('./config/db');
+
+const app = express();
+connectDb();
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined.');
     process.exit(1);
 }
 
-
-connectDB();
-
-app.use(express.json());
-
-// Define Routes
-app.get('/', (req, res) => {
-    const deviceDetector = new DeviceDetector();
-    const userAgent = req.get('user-agent');
-    const device = deviceDetector.parse(userAgent);
-    const browser = device.client.name;
-    const type = device.device.type;
-    res.send(type);
-});
-app.use('/r', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+app.use(express.json())
+app.use('/redirection', require('./routes/redirection'));
+app.use('/api/urls', require('./routes/urls'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/report', require('./routes/report'));
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
