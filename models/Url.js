66b26b16bd63bd69c54f48a2bd9c68116c110ecc
mahdi <m.mahdisaeedi99@gmail.com>
2021-01-15 22:38:25 +0330

    Change URL schema

--- models/Url.js
+++ models/Url.js
@@ -1,44 +1,17 @@
-const { number } = require('joi');
 const mongoose = require('mongoose');
 
 const urlSchema = new mongoose.Schema({
-  creator_id: String,
+  creatorId: String,
   urlCode: String,
 
-  views:{
-    totalView: { type: Number, default: 0 },
-    deviceType: {
-      desktopView: { type: Number, default: 0 },
-      mobileView: { type: Number, default: 0 },
-      otherDeviceView: { type: Number, default: 0 },
-    },
-    browserType: {
-      chromeView: { type: Number, default: 0 },
-      safariView: { type: Number, default: 0 },
-      firefoxView: { type: Number, default: 0 },
-      operaView: { type: Number, default: 0 },
-      otherBrowserView: { type: Number, default: 0 },
-    },
-  },
+  views: [{
+    userId: String,
+    browser: String,
+    deviceType: String,
+    createdAt: { type: Date, default: Date.now }
+  }],
   
-  distinctViews:{
-    distinctTotalView: { type: Array, default: [] },
-    distinctDeviceType: {
-      desktopView: { type: Array, default: [] },
-      mobileView: { type: Array, default: [] },
-      otherDeviceView: { type: Array, default: [] },
-    },
-    distinctBrowserType: {
-      chromeView: { type: Array, default: [] },
-      safariView: { type: Array, default: [] },
-      firefoxView: { type: Array, default: [] },
-      operaView: { type: Array, default: [] },
-      otherBrowserView: { type: Array, default: [] },
-    },
-  
-  },
-  
-  date: { type: String, default: Date.now }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Url', urlSchema);
